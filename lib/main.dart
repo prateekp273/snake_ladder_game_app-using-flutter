060dcad1926e6fe290dc8f3c8da6ae43093d4552
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-09 01:03:52 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,95 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SnakeAndLadders(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class SnakeAndLadders extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _SnakeAndLaddersState createState() => _SnakeAndLaddersState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _SnakeAndLaddersState extends State<SnakeAndLadders> {
+  static final int boardSize = 10;
+  static final int maxDiceValue = 6;
+  List<int> snakePositions = [16, 47, 49, 56, 62, 64, 87, 93, 95, 98];
+  List<int> ladderStarts = [1, 4, 9, 21, 28, 36, 51, 71, 80];
+  List<int> ladderEnds = [38, 14, 31, 42, 84, 44, 67, 91, 100];
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  int playerPosition = 1;
+  int diceValue = 1;
+  bool isGameOver = false;
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void rollDice() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      diceValue = Random().nextInt(maxDiceValue) + 1;
+      playerPosition += diceValue;
+
+      if (playerPosition > 100) {
+        playerPosition = 100;
+      }
+
+      if (snakePositions.contains(playerPosition)) {
+        playerPosition = 1;
+      } else if (ladderStarts.contains(playerPosition)) {
+        int index = ladderStarts.indexOf(playerPosition);
+        playerPosition = ladderEnds[index];
+      }
+
+      if (playerPosition == 100) {
+        isGameOver = true;
+      }
+    });
+  }
+
+  void resetGame() {
+    setState(() {
+      playerPosition = 1;
+      diceValue = 1;
+      isGameOver = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Snake and Ladders'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Player Position: $playerPosition',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Dice Value: $diceValue',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 20),
+          if (isGameOver)
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Congratulations! You won!',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-          ],
-        ),
+          if (!isGameOver)
+            ElevatedButton(
+              onPressed: rollDice,
+              child: Text('Roll Dice'),
+            ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: resetGame,
+            child: Text('Reset Game'),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
