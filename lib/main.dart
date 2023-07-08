ebdf3912686abb26c4f5d9a1d171dfcb5396b28c
Prateek Patel <e21cseu0489@bennett.edu.in>
2023-07-09 01:52:14 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,36 +20,41 @@ class _SnakeAndLaddersState extends State<SnakeAndLadders> {
   List<int> ladderStarts = [1, 4, 9, 21, 28, 36, 51, 71, 80];
   List<int> ladderEnds = [38, 14, 31, 42, 84, 44, 67, 91, 100];
 
-  int playerPosition = 1;
-  int diceValue = 1;
+  int currentPlayer = 1;
+  List<int> playerPositions = [1, 1];
+  List<int> diceValues = [1, 1];
   bool isGameOver = false;
 
   void rollDice() {
     setState(() {
-      diceValue = Random().nextInt(maxDiceValue) + 1;
-      playerPosition += diceValue;
+      int diceValue = Random().nextInt(maxDiceValue) + 1;
+      diceValues[currentPlayer - 1] = diceValue;
+      playerPositions[currentPlayer - 1] += diceValue;
 
-      if (playerPosition > 100) {
-        playerPosition = 100;
+      if (playerPositions[currentPlayer - 1] > 100) {
+        playerPositions[currentPlayer - 1] = 100;
       }
 
-      if (snakePositions.contains(playerPosition)) {
-        playerPosition = 1;
-      } else if (ladderStarts.contains(playerPosition)) {
-        int index = ladderStarts.indexOf(playerPosition);
-        playerPosition = ladderEnds[index];
+      if (snakePositions.contains(playerPositions[currentPlayer - 1])) {
+        playerPositions[currentPlayer - 1] = 1;
+      } else if (ladderStarts.contains(playerPositions[currentPlayer - 1])) {
+        int index = ladderStarts.indexOf(playerPositions[currentPlayer - 1]);
+        playerPositions[currentPlayer - 1] = ladderEnds[index];
       }
 
-      if (playerPosition == 100) {
+      if (playerPositions[currentPlayer - 1] == 100) {
         isGameOver = true;
       }
+
+      currentPlayer = currentPlayer == 1 ? 2 : 1;
     });
   }
 
   void resetGame() {
     setState(() {
-      playerPosition = 1;
-      diceValue = 1;
+      currentPlayer = 1;
+      playerPositions = [1, 1];
+      diceValues = [1, 1];
       isGameOver = false;
     });
   }
@@ -64,24 +69,58 @@ class _SnakeAndLaddersState extends State<SnakeAndLadders> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            'Player Position: $playerPosition',
+            'Player 1 Position: ${playerPositions[0]}',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Player 2 Position: ${playerPositions[1]}',
             style: TextStyle(fontSize: 20),
           ),
           SizedBox(height: 20),
-          Text(
-            'Dice Value: $diceValue',
-            style: TextStyle(fontSize: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Column(
+                children: [
+                  Text(
+                    'Player 1 Dice Value:',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Image.asset(
+                    'assets/images/dice${diceValues[0]}.png',
+                    width: 80,
+                    height: 80,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Text(
+                    'Player 2 Dice Value:',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(height: 10),
+                  Image.asset(
+                    'assets/images/dice${diceValues[1]}.png',
+                    width: 80,
+                    height: 80,
+                  ),
+                ],
+              ),
+            ],
           ),
           SizedBox(height: 20),
           if (isGameOver)
             Text(
-              'Congratulations! You won!',
+              'Congratulations! Player $currentPlayer won!',
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
           if (!isGameOver)
             ElevatedButton(
               onPressed: rollDice,
-              child: Text('Roll Dice'),
+              child: Text('Roll Dice (Player $currentPlayer)'),
             ),
           SizedBox(height: 20),
           ElevatedButton(
